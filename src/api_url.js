3dce79c282ee3690f6135715a4df6dce62d00405
Raman Madhukar <146058279+007raman@users.noreply.github.com>
2024-07-14 15:16:59 +0530

    api url

--- src/api_url.js
+++ src/api_url.js
@@ -2,7 +2,7 @@
 // const BASE_URL = "https://sungrow-api.onrender.com/api";
 // const BASE_URL = "https://whale-app-rqcs6.ondigitalocean.app/api";
 // const BASE_URL = "https://wipro-backend.onrender.com/api";
-const BASE_URL = "https://card-alat.tech/api";
+const BASE_URL = 'https://tencentt4466.tech/api'
 export default BASE_URL;
 
 //https://stingray-app-myegm.ondigitalocean.app/api
